06d6385db5fcebec7d9e6714701e8f937458ff82
lkj0136 <75069497+lkj0136@users.noreply.github.com>
2024-09-02 15:58:46 +0900

    Add files via upload

---
+++ views/data_view.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QPushButton, QLabel
+
+
+class DataView(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+
+        self.data_input = QTextEdit()
+        self.send_button = QPushButton("Send Data")
+        self.multi_send_button = QPushButton("Multi Send Data")
+
+        self.repeat_count_label = QLabel("Repeat Count:")
+        self.repeat_count_input = QTextEdit()
+
+        self.repeat_interval_label = QLabel("Repeat Interval (ms):")
+        self.repeat_interval_input = QTextEdit()
+
+        self.received_data = QTextEdit()
+        self.received_data.setReadOnly(True)
+
+        layout.addWidget(self.data_input)
+        layout.addWidget(self.send_button)
+        layout.addWidget(self.multi_send_button)
+        layout.addWidget(self.repeat_count_label)
+        layout.addWidget(self.repeat_count_input)
+        layout.addWidget(self.repeat_interval_label)
+        layout.addWidget(self.repeat_interval_input)
+        layout.addWidget(self.received_data)
+
+        self.setLayout(layout)
---
+++ views/main_view.py
@@ -0,0 +1,30 @@
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+from ui.port_settings import PortSettings
+from ui.data_transfer import DataTransfer
+from ui.file_transfer import FileTransfer
+from ui.status_bar import StatusBar
+from ui.profile_settings import ProfileSettings
+
+
+class MainView(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Serial Manager")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.port_settings = PortSettings()
+        self.data_transfer = DataTransfer()
+        self.file_transfer = FileTransfer()
+        self.status_bar = StatusBar()
+        self.profile_settings = ProfileSettings()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.port_settings)
+        layout.addWidget(self.data_transfer)
+        layout.addWidget(self.file_transfer)
+        layout.addWidget(self.profile_settings)
+        layout.addWidget(self.status_bar)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
